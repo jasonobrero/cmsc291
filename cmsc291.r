ff11891613cb92e4b4d403c76ade145c562da08b
Rick Jason Obrero <rickjasonobrero@gmail.com>
2018-12-04 14:07:29 +0800

    Revert

--- cmsc291.r
+++ cmsc291.r
@@ -1,28 +1,29 @@
 #install.packages(c("plyr", "neuralnet"))
 
-# require(plyr)
-# require(neuralnet)
-# library(plyr)
-# library(neuralnet)
-# data = read.csv("data_new.csv", header = TRUE)
-# data.numeric = data.frame(sapply(data, function(x) as.numeric(as.character(x))))
-# 
-# data.train = data.numeric[,"Dataset"] == 0
-# data.valid = data.numeric[,"Dataset"] == 1
-# 
-# data.numeric$Dataset = NULL
-# 
-# max = apply(data.numeric, 2, max)
-# min = apply(data.numeric, 2, min)
-#  
-# data.scaled = as.data.frame(scale(data.numeric, center = min, scale = max - min))
-# data.NNtrain = data.scaled[data.train,]
-# data.NNtest = data.scaled[-data.train,]
-# 
-# mat = model.matrix(
-#     ~ ID + Size + Volume + WedgeCount + ClawCount + MaxDegree + AveDeg + Fill,
-#     data = data.NNtrain
-# )
+require(plyr)
+require(neuralnet)
+library(plyr)
+library(neuralnet)
+require("./testcases.r")
+data = read.csv("data_new.csv", header = TRUE)
+data.numeric = data.frame(sapply(data, function(x) as.numeric(as.character(x))))
+
+data.train = data.numeric[,"Dataset"] == 0
+data.valid = data.numeric[,"Dataset"] == 1
+
+data.numeric$Dataset = NULL
+
+max = apply(data.numeric, 2, max)
+min = apply(data.numeric, 2, min)
+
+data.scaled = as.data.frame(scale(data.numeric, center = min, scale = max - min))
+data.NNtrain = data.scaled[data.train == TRUE,]
+data.NNtest = data.scaled[data.train == FALSE,]
+
+mat = model.matrix(
+    ~ ID + Size + Volume + WedgeCount + ClawCount + MaxDegree + AveDeg + Fill,
+    data = data.NNtrain
+)
 
 set.seed(2)
 for (testcase in testcases) {
@@ -30,7 +31,6 @@ for (testcase in testcases) {
   NN = neuralnet(
     ID ~ + Size + Volume + WedgeCount + ClawCount + MaxDegree + AveDeg + Fill,
     mat,
-    algorithm = "sag",
     hidden = testcase[[1]],
     linear.output = FALSE,
     stepmax = 1e+7
